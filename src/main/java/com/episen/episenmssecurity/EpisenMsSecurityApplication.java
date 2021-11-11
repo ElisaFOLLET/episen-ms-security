91deb4682c53be6365accaaa1bc46945a4b101dc
ElisaFollet <elisa3.follet@gmail.com>
2021-11-11 14:24:11 +0100

    first commit

---
+++ src/main/java/com/episen/episenmssecurity/EpisenMsSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.episen.episenmssecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpisenMsSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpisenMsSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/episen/episenmssecurity/EpisenMsSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.episen.episenmssecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpisenMsSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
