d2f31e3f0955ae99de8afeeb57fef7d8bb1c0487
ElisaFollet <elisa3.follet@gmail.com>
2021-11-23 17:41:27 +0100

    Test pour le chemin d'accès vers le fichier server.p12 (5)

--- src/main/java/com/episen/episenmssecurity/setting/InfraSettings.java
+++ src/main/java/com/episen/episenmssecurity/setting/InfraSettings.java
@@ -9,10 +9,14 @@ import java.security.PrivateKey;
 import java.security.cert.Certificate;
 
 public class InfraSettings {
+
+    /**
+     * It is necessary to modify the path of the folder server.p12
+     * @return KeyPair
+     */
     public static KeyPair keyPairLoader(){
-//C:/Users/elisa/git-workspace/episen-ms-security/src/main/resources/keys/server.p12 en local
-        //File file = new File("episen-ms-security/src/main/resources/keys/server.p12");
-        try(FileInputStream is = new FileInputStream("episen-ms-security/src/main/resources/keys/server.p12")) {
+
+        try(FileInputStream is = new FileInputStream("C:/Users/elisa/git-workspace/episen-ms-security/src/main/resources/keys/server.p12")) {
 
             KeyStore kstore = KeyStore.getInstance("PKCS12");
             kstore.load(is, "episen".toCharArray());
