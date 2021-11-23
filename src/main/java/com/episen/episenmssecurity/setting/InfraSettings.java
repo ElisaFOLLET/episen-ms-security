6d2105b0d5687cae0be7ccad2929717645bde7b8
ElisaFollet <elisa3.follet@gmail.com>
2021-11-23 17:20:03 +0100

    Test pour le chemin d'accès vers le fichier server.p12 (3)

--- src/main/java/com/episen/episenmssecurity/setting/InfraSettings.java
+++ src/main/java/com/episen/episenmssecurity/setting/InfraSettings.java
@@ -1,5 +1,6 @@
 package com.episen.episenmssecurity.setting;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.security.Key;
 import java.security.KeyPair;
@@ -10,7 +11,8 @@ import java.security.cert.Certificate;
 public class InfraSettings {
     public static KeyPair keyPairLoader(){
 //C:/Users/elisa/git-workspace/episen-ms-security/src/main/resources/keys/server.p12 en local
-        try(FileInputStream is = new FileInputStream("./episen-ms-security/src/main/resources/keys/server.p12")) {
+        File file = new File("src/main/resources/keys/server.p12");
+        try(FileInputStream is = new FileInputStream(file.getAbsolutePath())) {
 
             KeyStore kstore = KeyStore.getInstance("PKCS12");
             kstore.load(is, "episen".toCharArray());
