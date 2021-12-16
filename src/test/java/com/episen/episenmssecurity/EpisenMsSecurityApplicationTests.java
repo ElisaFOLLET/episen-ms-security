9153a1b9953998a9f72141611dcd8a0238f9bf2a
ElisaFollet <elisa3.follet@gmail.com>
2021-12-16 23:46:15 +0100

    Test pour le chemin d'accès vers le fichier server.p12 (7)

--- src/test/java/com/episen/episenmssecurity/EpisenMsSecurityApplicationTests.java
+++ src/test/java/com/episen/episenmssecurity/EpisenMsSecurityApplicationTests.java
@@ -1,5 +1,5 @@
 package com.episen.episenmssecurity;
-
+/*
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,3 +11,4 @@ class EpisenMsSecurityApplicationTests {
 	}
 
 }
+*/
