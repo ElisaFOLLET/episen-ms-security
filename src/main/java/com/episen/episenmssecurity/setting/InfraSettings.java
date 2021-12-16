5d846817fa9080af984b7d15ff7a73f8ec7786d6
ElisaFOLLET <61044693+ElisaFOLLET@users.noreply.github.com>
2021-12-16 22:21:22 +0100

    Update InfraSettings

--- src/main/java/com/episen/episenmssecurity/setting/InfraSettings.java
+++ src/main/java/com/episen/episenmssecurity/setting/InfraSettings.java
@@ -16,7 +16,7 @@ public class InfraSettings {
      */
     public static KeyPair keyPairLoader(){
 
-        try(FileInputStream is = new FileInputStream("C:/Users/elisa/git-workspace/episen-ms-security/src/main/resources/keys/server.p12")) {
+        try(FileInputStream is = new FileInputStream($PATH)) {
 
             KeyStore kstore = KeyStore.getInstance("PKCS12");
             kstore.load(is, "episen".toCharArray());
